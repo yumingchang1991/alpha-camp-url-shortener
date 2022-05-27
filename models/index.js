cc97c7c5e8be80f36f9ff8f73b3d92415611f8bb
ChillDiver <yumingchang1991@gmail.com>
2022-05-27 13:06:46 +0800

    feat: add data fetching oprerations for routes /urls

---
+++ models/index.js
@@ -0,0 +1,59 @@
+const Url = require('./url')
+const letters = 'abcdefghijklmnopqrstuvwxyz'
+const numbers = '1234567890'
+const randomList = [...(letters + letters.toUpperCase() + numbers)]
+
+const model = {
+  async returnUrl(originalUrl) {
+    const urlFound = Object.create(null)
+    await Url
+      .findOneAndUpdate(
+        { originalUrl },
+        {
+          $setOnInsert: {
+            originalUrl: originalUrl,
+            shortenPath: model.generateUniqueShortenPath()
+          },
+          $inc: { useCount: 1 }
+        },
+        {
+          new: true,
+          upsert: true,
+        }
+      )
+      .lean()
+      .then(url => Object.assign(urlFound, url))
+    return urlFound
+  },
+
+  generateUniqueShortenPath() {
+    let uniqueShortenPath = model.generateShortenPath()
+    const existingUrls = model.returnUrls() || []
+    const existingShortenPaths = existingUrls.length > 0 ? existingUrls.map(url => url.shortenPath) : []
+    while (existingShortenPaths.includes(uniqueShortenPath)) {
+      uniqueShortenPath = model.generateShortenPath()
+    }
+    return uniqueShortenPath
+  },
+
+  generateShortenPath() {
+    const newShortenPath = []
+    while (newShortenPath.length < 5) {
+      newShortenPath.push(
+        randomList[Math.floor(Math.random() * randomList.length)]
+      )
+    }
+    return newShortenPath.join('')
+  },
+
+  async returnUrls() {
+    let result = []
+    await Url
+      .find()
+      .lean()
+      .then(data => result = data.slice())
+    return result
+  }
+}
+
+module.exports = model
