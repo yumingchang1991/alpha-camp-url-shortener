797f391b940e82ca0a257d958f3fe8c492db9a90
ChillDiver <yumingchang1991@gmail.com>
2022-05-27 13:05:59 +0800

    feat: add client JS v1

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,48 +1,170 @@
+const HEAD = document.getElementsByTagName('HEAD')[0]
 const BODY = document.getElementsByTagName('BODY')[0]
 const icon = document.getElementsByTagName('i')[0]
-const submitButton = document.querySelector('button[type="submit"]')
-const app = document.querySelector('.app')
+const inputUrl = document.querySelector('input[type="url"]')
+const button = document.querySelector('button')
+
+const utility = {
+  isUrlValid() {
+    return inputUrl.value.length > 0
+  },
+
+  isResponseValid(res) {
+    const keysToHave = ['originalUrl', 'shortenPath']
+    const keys = Object.keys(res.data)
+    return keysToHave.reduce((result, keyToHave) => {
+      return keys.some(key => key === keyToHave)
+    }, true)
+  }
+}
+
 const model = {
   currentState: 'awaiting',
+  urlReturned: {},
+
   stateStore: {
     awaiting: 'awaiting',
     processing: 'processing',
     completed: 'completed'
   },
+
   classStore: {
     rotate: 'rotate-180',
     roll: 'roll',
     backgroundGray: 'background-gray',
     processing: 'processing'
   },
+
+  async fetchingData() {
+    const path = '/urls'
+    const APIEndpoint = window.location.origin + path
+    let urlReturned = Object.create(null)
+    await axios
+      .post(APIEndpoint, { originalUrl: inputUrl.value })
+      .then(response => {
+        if (!utility.isResponseValid(response)) {
+          // error handling will go here
+          return console.log('data returned from API is not complete')
+        }
+        Object.assign(urlReturned, response.data)
+      })
+      .catch(e => console.error(e))
+
+    return urlReturned
+  }
 }
 
-window.addEventListener('load', () => {
-  submitButton.addEventListener('mouseenter', becomeFocus)
-  submitButton.addEventListener('mouseleave', leaveFocus)
-  submitButton.addEventListener('click', submitURL)
-  console.log('GREAT! ~ URL Shortener is loaded')
-})
+const view = {
+  addCSSLinkToHead() {
+    const id = 'index-css'
+    if (!document.getElementById(id)) {
+      const link = document.createElement('link')
+      HEAD.appendChild(link)
+      link.href = '/stylesheets/index.css'
+      link.type = 'text/css'
+      link.rel = 'stylesheet'
+    }
+  },
 
-function becomeFocus (e) {
-  BODY.classList.add(model.classStore.backgroundGray)
-  icon.classList.add(model.classStore.rotate)
+  renderShortenPath(url) {
+
+  },
+  renderCopyButton() {
+
+  },
+
+  toggleProcessingAnimation(isEnteringProcessing = true) {
+    if (isEnteringProcessing) {
+      icon.classList.remove(model.classStore.rotate)
+      icon.classList.add(model.classStore.roll)
+      view.eventListeners.toggleFocusEffect(false)
+      button.setAttribute('disabled', true)
+      button.textContent = model.currentState
+      button.classList.add(model.classStore.processing)
+      return
+    }
+    icon.classList.remove(model.classStore.roll)
+    view.eventListeners.toggleFocusEffect(true)
+    button.setAttribute('disabled', false)
+    button.textContent = 'Click to Copy'
+    button.classList.remove(model.classStore.processing)
+  },
+
+  eventHandlers: {
+    addFocusEffect(e) {
+      BODY.classList.add(model.classStore.backgroundGray)
+      icon.classList.add(model.classStore.rotate)
+    },
+    removeFocusEffect(e) {
+      BODY.classList.remove(model.classStore.backgroundGray)
+      icon.classList.remove(model.classStore.rotate)
+    },
+    submitURL(e) {
+      if (!utility.isUrlValid()) {
+        alert('Please enter a valid url')
+        return
+      }
+      model.currentState = model.stateStore.processing
+      controller.run(model.currentState)
+    }
+  },
+
+  eventListeners: {
+    addAwaitingListener() {
+      view.eventListeners.toggleFocusEffect(true)
+      button.addEventListener('click', view.eventHandlers.submitURL)
+    },
+
+    toggleFocusEffect(toBeAdded = true) {
+      if (toBeAdded) {
+        button.addEventListener('mouseenter', view.eventHandlers.addFocusEffect)
+        button.addEventListener('mouseleave', view.eventHandlers.removeFocusEffect)
+        return
+      }
+      button.removeEventListener('mouseenter', view.eventHandlers.addFocusEffect)
+      button.removeEventListener('mouseleave', view.eventHandlers.removeFocusEffect)
+    }
+  }
 }
 
-function leaveFocus (e) {
-  BODY.classList.remove(model.classStore.backgroundGray)
-  icon.classList.remove(model.classStore.rotate)
+const controller = {
+  run(currentState) {
+    switch (currentState) {
+      case '':
+      case model.stateStore.awaiting:
+        controller.runAwaiting()
+        break
+      case model.stateStore.processing: {
+        controller.runProcessing()
+        break
+      }
+      case model.stateStore.completed:
+        controller.runCompleted()
+        break
+      default:
+
+    }
+  },
+
+  runAwaiting() {
+    view.addCSSLinkToHead()
+    view.eventListeners.addAwaitingListener()
+    console.log(`GREAT! ~ URL Shortener is initialized to: ${model.currentState}`)
+  },
+
+  async runProcessing() {
+    view.toggleProcessingAnimation(true)
+    model.urlReturned = await model.fetchingData()
+    model.currentState = model.stateStore.completed
+    controller.run(model.currentState)
+  },
+
+  runCompleted() {
+    view.toggleProcessingAnimation(false)
+    view.eventListeners.toggleFocusEffect(true)
+    view.renderShortenPath(model.urlReturned)
+    view.renderCopyButton()
+  }
 }
 
-function submitURL (e) {
-  model.currentState = model.stateStore.processing
-  
-  icon.classList.remove(model.classStore.rotate)
-  icon.classList.add(model.classStore.roll)
-
-  submitButton.removeEventListener('mouseenter', becomeFocus)
-  submitButton.removeEventListener('mouseleave', leaveFocus)
-  submitButton.setAttribute('disabled', true)
-  submitButton.textContent = model.currentState
-  submitButton.classList.add(model.classStore.processing)
-}
+controller.run(model.currentState)
